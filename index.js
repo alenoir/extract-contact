6538aba92a5ddb138ea2a97dfc137a4d31dd1034
Antoine Lenoir <antoine.alenoir@gmail.com>
2016-09-22 15:27:42 +0200

    add extract for email and url

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
+function extractEmails(string) {
+  var re = /(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))/g;
+  var emails = string.match(re) || [];
+  return emails.filter(function(elem, pos) {
+    return emails.indexOf(elem) == pos;
+  });
+}
+
+function extractUrls(string) {
+  var re = /(https?:\/\/(?:www\.|(?!www))[^\s!>!<\.]+\.[^\s!>!<]{2,}|www\.[^\s!>!<]+\.[^\s!>!<]{2,})/gi;
+  var urls = string.match(re) || [];
+  return urls.filter(function(elem, pos) {
+    return urls.indexOf(elem) == pos;
+  });
+}
+
 module.exports = function(string) {
   var result = {
-    emails: [],
-    phones: [],
-    urls: []
+    emails: extractEmails(string),
+    urls: extractUrls(string)
   };
+
+  return result;
 }
--- test/test.js
+++ test/test.js
@@ -1,9 +1,39 @@
-var assert = require('assert');
+var extratContact = require('../index');
+var should = require('should');
 
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal(-1, [1,2,3].indexOf(4));
-    });
+describe('Extract Contact', function() {
+  it('should extract nothing', function() {
+    var string = 'Curabitur blandit tempus porttitor.';
+    var result = extratContact(string);
+    result.should.have.property('emails', []);
+    result.should.have.property('urls', []);
+  });
+
+  it('should extract one email', function() {
+    var string = 'Curabitur blandit test@gmail.com tempus porttitor.';
+    var result = extratContact(string);
+    result.should.have.property('emails', ['test@gmail.com']);
+    result.should.have.property('urls', []);
+  });
+
+  it('should extract multiple uniques emails', function() {
+    var string = 'Curabitur test@gmail.com blandit test2@gmail.com tempus porttitor test2@gmail.com.';
+    var result = extratContact(string);
+    result.should.have.property('emails', ['test@gmail.com', 'test2@gmail.com']);
+    result.should.have.property('urls', []);
+  });
+
+  it('should extract url', function() {
+    var string = 'Curabitur http://test.com blandit tempus porttitor.';
+    var result = extratContact(string);
+    result.should.have.property('emails', []);
+    result.should.have.property('urls', ['http://test.com']);
+  });
+
+  it('should extract multiple uniques urls', function() {
+    var string = 'Curabitur http://test.com blandit http://test2.com tempus porttitor http://test2.com';
+    var result = extratContact(string);
+    result.should.have.property('emails', []);
+    result.should.have.property('urls', ['http://test.com', 'http://test2.com']);
   });
 });
